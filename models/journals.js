8552286924c261375dc01fa740b28bdd7b9bc736
Ashley Oliver <jeangray218@gmail.com>
2023-05-30 16:04:53 -0400

    fixed small errors in routes/all routes are workinggit add .

--- models/journals.js
+++ models/journals.js
@@ -4,9 +4,12 @@ const journalSchema = new mongoose.Schema({
     title: { type: String, required: true },
     date:  { type: String, required: true },
     entry: { type: String, required: true },
-    goal1: { type: String, isCompleted:{ type:Boolean, default: false} },
-    goal2: { type: String, isCompleted:{ type:Boolean, default: false} },
-    goal3: { type: String, isCompleted:{ type:Boolean, default: false} }
+    goal1: { type: String},
+    goal2: { type: String}, 
+    goal3: { type: String}, 
+    goal1IsCompleted:{ type:Boolean, default: false},
+    goal2IsCompleted:{ type:Boolean, default: false},
+    goal3IsCompleted:{ type:Boolean, default: false}
 });
 
 const Journals = mongoose.model('Journal', journalSchema)
